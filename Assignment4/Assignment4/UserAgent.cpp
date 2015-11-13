98d04238c2bcb40de444f52a26445cd9bd7ac949
Matt Wright <wright.matt92@gmail.com>
2015-11-13 16:28:47 -0600

    Finished working on files, linked list does not lopp however

--- Assignment4/Assignment4/UserAgent.cpp
+++ Assignment4/Assignment4/UserAgent.cpp
@@ -1,6 +1,7 @@
 #include        <cstdlib>
 #include        <iostream>
 #include        <string>
+#include		<regex>
 
 using namespace std;
 
@@ -11,4 +12,58 @@ using namespace std;
 #include        "LocationIterator.h"
 #include        "Agent.h"
 #include        "Game.h"
+
+UserAgent::UserAgent(LocationList&	locationList
+	) :
+	  	Agent(locationList)
+	  	{
+	  	  cout << "Your name? ";
+	  	  getline(cin,agentName_);
+	  	}
+
+UserAgent::~UserAgent()
+{
+	delete(&agentName_);
+}
+
+const string&	UserAgent::getName()const
+{
+	return(agentName_);
+}
+
+action_t	UserAgent::getAction(Game& game)
+{
+	regex left("(([g|G][o|O]?)? ?[leftLEFT]+)");
+	regex right("(([g|G][o|O]?)? ?[rightRIGHT]+)");
+	regex quit("[quitQUIT]+");
+	action_t action;
+
+	string	input;
+	while (true)
+	{
+		cout << "Your choice (left/right/quit)? ";
+		getline(cin, input);
+		if (input.size() == 0)
+		{
+			cout << "I'm sorry?\n";
+			continue;
+		}
+		else
+		{
+			if (regex_match(input, left)) {
+				action = GO_LEFT_ACTION;
+				break;
+			}
+			else if (regex_match(input, right)) {
+				action = GO_RIGHT_ACTION;
+				break;
+			}
+			else if (regex_match(input, quit)) {
+				action = QUIT_ACTION;
+				break;
+			}
+		}
+	}
+	return action;
+}
 
